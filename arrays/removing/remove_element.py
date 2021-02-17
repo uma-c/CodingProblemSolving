d292bc60564eedda95f1c64e388170f33e37ad91
Uma Chowtoori <chowtoori@live.com>
2021-02-17 08:13:57 +0530

    array problems solved

--- arrays/removing/remove_element.py
+++ arrays/removing/remove_element.py
@@ -1,4 +1,15 @@
 from typing import List
 
 def removeElement(nums: List[int], val: int) -> int:
-    pass
+    write_pos = 0
+    for num in nums:
+        if num != val:
+            nums[write_pos] = num
+            write_pos += 1
+    return write_pos
+
+if __name__ == "__main__":
+    nums = [3,2,2,3]
+    val = 3
+    print(removeElement(nums, val))
+    print(nums)
